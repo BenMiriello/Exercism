ce02b40b4d8caa0508cc0c1587751139e091a151
BenMiriello <benmiriello@gmail.com>
2021-03-01 23:38:10 -0500

    Add series problem + passing first test

---
+++ ruby/series/series.rb
@@ -0,0 +1,25 @@
+=begin
+Write your code for the 'Series' exercise in this file. Make the tests in
+`series_test.rb` pass.
+
+To get started with TDD, see the `README.md` file in your
+`ruby/series` directory.
+=end
+
+class Series
+  def initialize(numbers)
+    @@numbers = numbers.split('').map { |sn| sn.to_i }
+  end
+
+  def slices(n)
+    @slices = []
+    i = 0
+
+    while i < @@numbers.length - n + 1
+      @slices << @@numbers[i..i + n - 1]
+      i += 1
+    end
+
+    @slices.map { |set| set.join('').to_s }
+  end
+end
