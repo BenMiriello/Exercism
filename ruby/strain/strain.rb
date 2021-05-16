70060e18b389a4085b4d2119f9655cbf2021ebf0
BenMiriello <benmiriello@gmail.com>
2021-05-16 12:39:14 -0400

    Implement keep

--- ruby/strain/strain.rb
+++ ruby/strain/strain.rb
@@ -5,4 +5,18 @@ Write your code for the 'Strain' exercise in this file. Make the tests in
 To get started with TDD, see the `README.md` file in your
 `ruby/strain` directory.
 =end
+class Array
+  def keep
+    mapped_array = []
+    counter = 0
+    until counter == self.size
+      mapped_array.push(self[counter]) if yield self[counter]
+      counter += 1
+    end
+    mapped_array
+  end
 
+  def discard
+    yield self
+  end
+end
