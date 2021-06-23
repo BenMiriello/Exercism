c7a3d611f8e226e472def18442c13952e0d6c4b3
BenMiriello <benmiriello@gmail.com>
2021-06-23 00:16:38 -0400

    Solve raindrops

--- ruby/raindrops/raindrops.rb
+++ ruby/raindrops/raindrops.rb
@@ -6,3 +6,13 @@ To get started with TDD, see the `README.md` file in your
 `ruby/raindrops` directory.
 =end
 
+class Raindrops
+  def self.convert(n)
+    message = ''
+    message += 'Pling' if n % 3 == 0
+    message += 'Plang' if n % 5 == 0
+    message += 'Plong' if n % 7 == 0
+    message = n.to_s if message.length.zero?
+    message
+  end
+end
